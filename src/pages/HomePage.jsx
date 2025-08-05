78f0260585104727cd81ec69aed25f8adc566f60
LONECODER1 <adityagupta1112004vns@gmail.com>
2025-08-05 19:17:45 +0530

    some more changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Logo from '/assets/logo.jpg';
 
 const HomePage = () => {
     const navigate = useNavigate();
